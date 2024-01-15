ed89a8ce702933602221918e40699c95a38ba45e
serichannu <serina.chappy@icloud.com>
2024-01-15 15:12:59 +0900

    エラーメッセージの日本語化設定

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,6 +45,10 @@
                 </a>
             @endif --}}
         <div class="flex items-center justify-end mt-2">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                会員登録
+            </a>
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
