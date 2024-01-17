c9f96c014de192ee78e1114e82f8f57817bf0df7
serichannu <serina.chappy@icloud.com>
2024-01-17 15:28:09 +0900

    フッターのアプリ名を変更

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="">
     <div class="bg-light pt-3 pb-1 text-center">
-        <p>&copy; 2024 Your App Name. All rights reserved.</p>
+        <p>&copy; 2024 Hands App. All rights reserved.</p>
     </div>
 </footer>
