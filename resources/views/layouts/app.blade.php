59a3e5fb50f0d583c3550c312792b9001400a975
serichannu <serina.chappy@icloud.com>
2024-01-17 17:17:41 +0900

    script追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,12 @@
 
         <link rel="stylesheet" href="{{ asset('css/hands-app.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/ja.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-    </head>
+        </head>
     <body class="d-flex flex-column min-vh-100">
     <div id="app">
         @component('components.header')
@@ -24,8 +28,6 @@
             <title>{{ config('app.name', 'Laravel') }}</title>
             @component('components.footer')
             @endcomponent
-            <!-- Scripts -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
         </main>
     </div>
