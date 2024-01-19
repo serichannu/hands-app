3e1e48b116b7deabc5613387c9cf11711ff95f6d
serichannu <serina.chappy@icloud.com>
2024-01-19 11:47:58 +0900

    ルート修正

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link text-decoration-none" href="{{ route('seat.index') }}">
+                    <a class="nav-link text-decoration-none" href="{{ route('seats.index') }}">
                         <i class="fas fa-chair"></i>席替え
                     </a>
                 </li>
@@ -23,7 +23,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link text-decoration-none" href="{{ route('mypage.index') }}">
+                    <a class="nav-link text-decoration-none" href="{{ route('mypages.index') }}">
                         <i class="fas fa-user"></i>マイページ
                     </a>
                 </li>
