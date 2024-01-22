ffc8c9cd2d9b8fee9cea72730d40f145ed05cd2a
serichannu <serina.chappy@icloud.com>
2024-01-22 19:41:42 +0900

    出席番号入力ビューを実装

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,1 +1,22 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h6 class="mt-3 mb-3">学生の人数（出席番号）を設定</h6>
+
+        <form action="{{ route('students.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="studentNum">学生の人数：</label>
+                <select class="form-control w-25 mb-3" name="studentNum" id="studentNum" required>
+                    @for ($i = 1; $i <= 40; $i++)
+                        <option value="{{ $i }}">{{ $i }}人</option>
+                    @endfor
+                </select>
+            </div>
+            <button type="submit" class="btn btn-secondary mb-3">保存</button>
+        </form>
+
+    </div>
+
+@endsection
