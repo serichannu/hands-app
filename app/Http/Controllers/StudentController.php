f8bbfc0074f601faf7964fa2e31dbfd0e2def42d
serichannu <serina.chappy@icloud.com>
2024-01-23 15:03:12 +0900

    ルート名とメソッド名修正

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -9,8 +9,10 @@ use Illuminate\Support\Facades\Auth;
 
 class StudentController extends Controller
 {
-    public function index() {
-        return view('students.index');
+    public function create() {
+        $students = Student::where('user_id', Auth::id())->get();
+        return view('students.create', [
+            'student' => $students]);
     }
 
     public function store(Request $request) {
@@ -28,4 +30,27 @@ class StudentController extends Controller
         }
         return redirect()->route('seats.index')->with('success', '学生の人数(出席番号）の設定が完了しました。');
     }
+
+    // public function edit(Student $student) {
+    //     $student = Student::all();
+    //     return view('students.edit', compact('student'));
+    // }
+
+    // public function update(Request $request, Student $student) {
+    //     $student = Student::all();
+    //     $request->validate([
+    //         'studentNum' => 'required|numeric|min:1|max:40',
+    //     ]);
+
+    //     $studentNum = $request->input('studentNum');
+
+    //     for ($i = 1; $i <= $studentNum; $i++) {
+    //         $student = new Student();
+    //         $student->number = $i;
+    //         $student->user_id = Auth::id();
+    //         $student->update();
+    //     }
+
+    //     return redirect()->route('seats.index')->with('success', '学生の人数(出席番号）の設定が完了しました。');
+    // }
 }
