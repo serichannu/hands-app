a69c89a5d5eff7fd54fd8ebce8753be329134898
serichannu <serina.chappy@icloud.com>
2024-01-23 15:02:24 +0900

    スタイル変更

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -14,13 +14,13 @@
                     @endfor
                 </select>
             </div>
-            <button type="submit" class="btn btn-secondary mb-3">保存</button>
-        </form>
-        <h6 class="mt-3 mb-3">学生の一覧</h6>
-
-            <div>
-                <a href="{{ route('students.edit') }}">編集</a>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-success mr-3"><i class="far fa-save"></i>保存</button>
+                {{-- <a href="{{ route('students.edit') }}" class="btn btn-primary" style="margin-left: 30px;">
+                    <i class="far fa-edit"></i>編集
+                </a> --}}
             </div>
+        </form>
     </div>
 
 @endsection
