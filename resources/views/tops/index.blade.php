edbf2c2aa742421f08efd801f670f950bf702609
serichannu <serina.chappy@icloud.com>
2024-01-29 15:12:18 +0900

    日付変更機能削除

--- resources/views/tops/index.blade.php
+++ resources/views/tops/index.blade.php
@@ -4,7 +4,6 @@
     <div class="container mb-1 mt-2">
         <div class="d-flex align-items-center">
             <p class="mt-3" id="currentDate">{{ now()->toDateString() }}</p>
-            <button id="openDatePicker" class="btn btn-light ms-3"><i class="far fa-calendar-alt"></i> 日付変更</button>
             <p class="mt-3 ms-3">教科：<span id="selectedSubject"></span></p>
 
             {{-- セレクタ --}}
@@ -30,23 +29,6 @@
     </div>
 
 
-    <!-- モーダル -->
-    <div class="modal fade" id="datePickerModal" tabindex="-1" aria-labelledby="datePickerModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="datePickerModalLabel">日付を選択してください</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <input type="text" id="datepicker" class="form-control">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">決定</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
     {{-- 座席情報 --}}
 
@@ -98,30 +80,10 @@
     @endif
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            // Flatpickr
-            flatpickr("#datepicker", {
-                dateFormat: "Y-m-d",
-                onClose: function (selectedDates, dateStr) {
-                    // 日付選択
-                    console.log(dateStr);
-                    // 日付の表示更新
-                    document.getElementById('currentDate').textContent = dateStr;
-                }
-            });
-
-            // モーダル
-            document.getElementById('openDatePicker').addEventListener('click', function () {
-                var myModal = new bootstrap.Modal(document.getElementById('datePickerModal'));
-                myModal.show();
-            });
-
             // セレクタ
             document.getElementById('subjectSelector').addEventListener('change', function () {
                 let selectedSubject = this.value;
                 document.getElementById('selectedSubject').textContent = selectedSubject;
             });
-        });
-
     </script>
 @endsection
