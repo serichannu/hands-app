6870ac37e4a750cd3c25daa3e4755f9eb7091915
serichannu <serina.chappy@icloud.com>
2024-01-31 12:15:51 +0900

    集計機能の実装

--- app/Http/Controllers/StaticsController.php
+++ app/Http/Controllers/StaticsController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Counter;
+use App\Models\Student;
+use App\Models\Subject;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StaticsController extends Controller
 {
     public function index() {
-        return view('statics.index');
+        $counterData = Counter::select('subject_id', 'student_id', 'date', DB::raw('SUM(count) as total_count'))
+        ->groupBy('subject_id', 'student_id', 'date')
+        ->get();
+        $students = Student::all();
+        $subjects = Subject::all();
+
+
+        return view('statics.index', compact('counterData', 'students', 'subjects'));
     }
 }
--- resources/views/statics/index.blade.php
+++ resources/views/statics/index.blade.php
@@ -1,1 +1,36 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="container mt-2">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>学生番号</th>
+                    @foreach ($subjects as $subject)
+                        <th>{{ $subject->name }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($students as $student)
+                    <tr>
+                        <td id="studentTd" style="{{ $student->number % 2 == 0 ? 'background-color: #d1e0f9;' : '' }}">{{ $student->number }}</td>
+                        @foreach ($subjects as $subject)
+                            <td style="{{ $student->number % 2 == 0 ? 'background-color: #d1e0f9' : '' }}">
+                                @php
+                                    $foundCounter = $counterData->first(function ($counter) use ($student, $subject) {
+                                        return $counter->student_id == $student->id && $counter->subject_id == $subject->id;
+                                    });
+
+                                    $totalCount = $foundCounter ? $foundCounter->total_count : 0;
+                                @endphp
+
+                                {{ $totalCount }}
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
