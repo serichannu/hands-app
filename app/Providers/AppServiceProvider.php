a8f37ac7bb239402a6eb4ccbabc96ca265fcf77a
serichannu <serina.chappy@icloud.com>
2024-02-08 12:45:12 +0900

    Debugbar無効

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
         if (App::environment(['production'])) {
             URL::forceScheme('https');
         }
+
+        \Debugbar::disable();
     }
 }
